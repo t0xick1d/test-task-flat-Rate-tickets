683a71148d80df9368654c70f6a32e95fd56bf27
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-07-24 13:05:04 +0300

    add doenv and create envoiroment for database

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 
-const contactsRouter = require('./routes/api/tickets');
+const ticketsRouter = require('./routes/api/tickets');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/tickets', ticketsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
